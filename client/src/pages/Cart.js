4e28ddbe0db9d0bbdd1c83235ac09309e21e1105
Brandon Diep <brandondiep2711@gmail.com>
2024-06-22 16:59:44 -0700

    added order summary to cart page

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,5 +1,10 @@
 import CartItems from "../components/CartItems";
 import { useCart } from '../context/CartContext'
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
+import { formatUSD } from '../utils/helpers';
+import paypal from "../assets/paypal.svg"
+
 
 const Cart = () => {
 
@@ -11,15 +16,35 @@ const Cart = () => {
       <section className='col'>
         <h2>Your Cart</h2>
         <div className='cart-options mt-5 d-flex'>
-          <h4 className='flex-fill cart-in p-3'>In Cart ({cartQuantity})</h4>
-          <h4 className='flex-fill cart-save p-3'>Saved For Later (0)</h4>
+          <h4 className='flex-fill cart-in p-3 mb-0'>In Cart ({cartQuantity})</h4>
+          <h4 className='flex-fill cart-save p-3 mb-0'>Saved For Later (0)</h4>
         </div>
         <CartItems cartItems={cartItems} cartImg={'cart-product-img-small'}/>
 
       </section>
       <section className='col-4'>
-       <h1>che c</h1>
+        <span className="order-summary mb-3">Order Summary</span>
+
+        <div className='d-flex justify-content-between'>
+            <span>Subtotal</span>
+            <span>{formatUSD(cartTotalPrice)}</span>
+        </div>
+        <div className='d-flex justify-content-between'>
+            <span>Shipping Cost</span>
+            <span>Free</span>
+        </div>
+        <div className='mt-3 pb-2 d-flex justify-content-between checkout-total'>
+            <h5>Total</h5>
+            <span className='cart-total-price'>{formatUSD(cartTotalPrice)}</span>
+        </div>
+
+        <Link to='/cart' className='mt-3 checkout d-grid gap-3'>
+          <Button variant="danger" size="lg">Checkout</Button>
+          <button type="button" class="btn btn-outline-secondary btn-lg"><img className="paypal-btn" src={paypal} alt="paypal" /></button>
+        </Link>
       </section>
+
+      
       
     </div>
   )
