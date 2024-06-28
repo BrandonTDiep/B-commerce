f692694b57b0338b91588626b9d900812c696954
Brandon Diep <brandondiep2711@gmail.com>
2024-06-28 16:13:56 -0700

    Revert "added routes for login and signup"
    This reverts commit ad2e2b223a225d298c009277efbddf2e8c67aa70.

--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/auth");
-
-// GET searched products
-router.get("/login", authController.getLogin);
-router.get("/signup", authController.getSignup);
-
-module.exports = router;
--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const path = require("path");
 const productRoutes = require("./routes/products");
 const categoryRoutes = require("./routes/categories");
 const searchRoutes = require("./routes/searches");
-const authRoutes = require("./routes/auth");
 
 const mongoose = require('mongoose');
 const flash = require("express-flash");
@@ -37,7 +36,6 @@ app.use(flash());
 app.use('/api/products', productRoutes)
 app.use('/api/categories', categoryRoutes)
 app.use('/api/searches', searchRoutes)
-app.use('/api/auth', authRoutes)
 
 
 // 404 handler
